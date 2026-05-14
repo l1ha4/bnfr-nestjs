78304577a0e816da7ea247d418719038c7749be2
l1ha4 <110506867+l1ha4@users.noreply.github.com>
2026-05-14 23:49:42 +0300

    fix

--- src/core/router/router.ts
+++ src/core/router/router.ts
@@ -1,15 +1,16 @@
-import { AdminModule } from "@/app/admin/admin-module.module";
-import { DsBotModule } from "@/app/admin/ds-bot/ds-bot.module";
+import { AdminModule } from '@/app/admin/admin-module.module'
+import { DsBotModule } from '@/app/admin/ds-bot/ds-bot.module'
+import type { Routes } from '@nestjs/core'
 
-export const router = [
+export const router: Routes = [
+  {
+    path: 'admin',
+    module: AdminModule,
+    children: [
       {
-        path: 'admin',
-        module: AdminModule,
-        children: [
-          {
-            path: 'ds-bot',
-            module: DsBotModule,
-          },
-        ],
+        path: 'ds-bot',
+        module: DsBotModule,
       },
-    ]
+    ],
+  },
+]
